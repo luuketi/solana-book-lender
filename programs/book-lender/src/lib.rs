059a542b6718ee661f9a7d2edeb6769dd4ab7c4a
lucas <paradisi.lucas@gmail.com>
2024-08-21 21:24:22 -0300

    Adding lend_book instruction

--- programs/book-lender/src/lib.rs
+++ programs/book-lender/src/lib.rs
@@ -1,18 +1,46 @@
 use anchor_lang::prelude::*;
 
+const ANCHOR_DISCRIMINATOR: usize = 8;
+
 declare_id!("8gfVjrhzX32iP1YBN9sqNrUnKt8Gzt8P3RnmPgwprxU5");
 
 #[program]
 pub mod book_lender {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn lend_book(ctx: Context<Lending>, from: Pubkey, to: Pubkey, ISBN: String, title: String ) -> Result<()> {
+        *ctx.accounts.lend = Lend {
+            from,
+            to,
+            ISBN,
+            title
+        };
+
+        msg!("Book {ISBN} - {title} lent from: {from} to: {to}");
         Ok(())
     }
-
-
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Lending<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR + Lend::INIT_SPACE,
+    )]
+    pub lend: Account<'info, Lend>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Lend {
+    pub from: Pubkey,
+    pub to: Pubkey,
+    #[max_len(13)]
+    pub ISBN: String,
+    #[max_len(100)]
+    pub title: String,
+}
--- tests/book-lender.ts
+++ tests/book-lender.ts
@@ -1,16 +1,35 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { BookLender } from "../target/types/book_lender";
+import { Keypair } from '@solana/web3.js';
+import { assert } from 'chai';
 
 describe("book-lender", () => {
-  // Configure the client to use the local cluster.
+  anchor.AnchorProvider.env();
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.BookLender as Program<BookLender>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it('Lend one book', async () => {
+    const source = new Keypair();
+    const destination = new Keypair();
+    const isbn = "1234";
+    const title = "Smalltalk-80: the language and its implementation";
+
+    await program.methods
+        .lendBook(source.publicKey, destination.publicKey, isbn, title)
+        .accounts({
+          lend: destination.publicKey,
+          payer: source.publicKey,
+        })
+        .signers([destination])
+        .rpc();
+
+      const lend = await program.account.lend.fetch(destination.publicKey);
+
+      assert(lend.from.toString() == source.publicKey);
+      assert(lend.to.toString() == destination.publicKey);
+      assert(lend.isbn == isbn);
+      assert(lend.title == title);
   });
 });
